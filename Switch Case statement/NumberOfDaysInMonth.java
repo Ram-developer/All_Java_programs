1865cb539de4dd68af1f78d0c060686d998c4b07
Ramprasad Mandal <69420934+Ram-developer@users.noreply.github.com>
2020-12-31 04:52:49 -1200

    Add files via upload

---
+++ Switch Case statement/NumberOfDaysInMonth.java
@@ -0,0 +1,41 @@
+//import javax.swing.*;
+
+public class NumberOfDaysInMonth {
+    public static void main(String[] args) {
+        //System.out.println(isLeapYear(2016));
+        boolean check=isLeapYear(2016);
+        System.out.println(check);
+        System.out.println(getDaysInMonth(12,10000));
+    }
+    public static boolean isLeapYear(int year) {
+        if (year>1 && year<9999) {
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        }
+        return false;
+    }
+public static int getDaysInMonth(int month,int year) {
+        if ((month<=0 || month>12 || year<=0 || year>9999)) {
+            return -1;
+        }
+        else
+            switch (month) {
+                case 1: return 31;
+                case 2: if (isLeapYear(year)) {
+                    return 29;
+                }
+                   else return 28;
+                case 3: return 31;
+                case 4: return 30;
+                case 5: return 31;
+                case 6: return 30;
+                case 7: return 31;
+                case 8: return 31;
+                case 9: return 30;
+                case 10: return 31;
+                case 11: return 30;
+                case 12: return 31;
+                default:return -1;
+            }
+
+}
+}
