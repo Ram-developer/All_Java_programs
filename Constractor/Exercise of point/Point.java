ad8461b7d4921c26276c0a9c76a655478c23e877
Ramprasad Mandal <69420934+Ram-developer@users.noreply.github.com>
2021-01-01 03:13:36 -1200

    Add files via upload

---
+++ Constractor/Exercise of point/Point.java
@@ -0,0 +1,32 @@
+public class Point {
+    private int x,y;
+
+    public Point() {
+
+    }
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public void setX(int x) {
+        this.x=x;
+    }
+    public void setY(int y) {
+        this.y=y;
+    }
+    public double distance() {
+        return Math.sqrt((this.x)*(this.x)+(this.y)*(this.y));
+    }
+    public double distance(int x,int y) {
+        return Math.sqrt((x -this.x)*(x -this.x)+(y -this.y)*(y -this.y));
+    }
+    public double distance(Point second) {
+    return Math.sqrt((second.x -this.x)*(second.x -this.x)+(second.y -this.y)*(second.y -this.y));
+    }
+}
